4b67150e943f7cf52a829c4536b75c17676be7df
lifuz <lifuzz@163.com>
2018-05-30 14:40:23 +0800

    init

---
+++ eureka-server/src/main/java/cn/lifuz/self/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package cn.lifuz.self.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName EurekaServerApplication
+ * @Description
+ * @Author lifuz
+ * @Email lifuzz@163.com
+ * @Date 2018/5/30 14:33
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
