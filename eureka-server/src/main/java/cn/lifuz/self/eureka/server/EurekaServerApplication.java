7f70770074729095d0807b362b5a6e9285eb9c56
lifuz <lifuzz@163.com>
2018-05-30 17:18:58 +0800

    sureka-server

--- eureka-server/src/main/java/cn/lifuz/self/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/lifuz/self/eureka/server/EurekaServerApplication.java
@@ -2,6 +2,7 @@ package cn.lifuz.self.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @ClassName EurekaServerApplication
@@ -11,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2018/5/30 14:33
  * @Version 1.0
  */
-
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
